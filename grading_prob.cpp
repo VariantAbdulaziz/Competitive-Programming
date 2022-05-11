56c1a637d51db6e754ea604e6991a9fdc3a224d7
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-11 19:24:53 +0300

    solved grading problem

---
+++ grading_prob.cpp
@@ -0,0 +1,17 @@
+// source: https://www.hackerrank.com/challenges/grading/submissions/code/269658590
+
+vector<int> gradingStudents(vector<int> grades) {
+
+    vector<int> finalMarks;
+    int n = grades.size();
+    
+    for(size_t i{}; i < n; i++){
+        int mod = grades[i]%5;
+        if(5 - mod >= 3 || mod == 0 || grades[i] < 38)
+            finalMarks.push_back(grades[i]);
+        else
+            finalMarks.push_back(grades[i] - mod + 5);
+    }
+    
+    return finalMarks;
+}
