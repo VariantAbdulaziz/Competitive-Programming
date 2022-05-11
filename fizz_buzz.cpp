edc53dd932160927626597b79f56829423db681d
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-11 19:24:36 +0300

    solved fizz buzz problem

---
+++ fizz_buzz.cpp
@@ -0,0 +1,24 @@
+// source: https://leetcode.com/problems/fizz-buzz/
+
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result{static_cast<size_t>(n)};
+        
+        for(int i = 1; i <= n; i ++){
+            
+            if(i%3==0 && i%5==0)
+                result[i-1] = "FizzBuzz";
+            else if(i%5==0)
+                result[i-1] = "Buzz";
+            else if(i%3==0)
+                result[i-1] = "Fizz";
+            else
+                result[i-1] = to_string(i);
+        }
+        
+        return result;
+        
+    }
+};
