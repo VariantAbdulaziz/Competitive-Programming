3ba757280a81e986280453f1e2a206038ed33df4
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-11 19:25:09 +0300

    solved domino problem

---
+++ domino.cpp
@@ -0,0 +1,12 @@
+// source: https://codeforces.com/problemset/problem/50/A
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << (n*m) / 2;
+    return 0;
+}
