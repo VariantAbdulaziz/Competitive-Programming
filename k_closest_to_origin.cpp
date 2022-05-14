b0fc115297a5d725e8bb99c5a5f9c7efc076585b
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-14 14:26:36 +0300

    solved k closest points to the origin problem

---
+++ k_closest_to_origin.cpp
@@ -0,0 +1,25 @@
+// source: https://leetcode.com/problems/k-closest-points-to-origin/
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
+        vector< vector<int> > res(k);
+        
+        sort(
+            points.begin(),
+            points.end(),
+            [](const vector<int>& a, const vector<int>& b){
+                
+                return a[0]*a[0] + a[1]*a[1] < b[0] * b[0] + b[1] * b[1];
+            }
+        );
+        
+        for(int i{}; i < k; i++){
+            
+            res[i] = points[i];
+        }
+        
+        return res;
+    }
+};
