d0fe3c66581d98273a995ae2c839e3af1ad229f7
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-14 16:31:46 +0300

    solved kth largest number in an array

---
+++ kth_largest.cpp
@@ -0,0 +1,22 @@
+// solution: https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/submissions/
+
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        
+        sort(nums.begin(),
+             nums.end(),
+             [](auto& a, auto& b){
+                if (a.size() == b.size()) {
+                    for (auto i = 0; i < a.size(); ++i) {
+                        if (a[i] == b[i])
+                            continue;
+                        return a[i] > b[i];
+                    }
+                }
+                return a.size() > b.size();
+             }
+        );
+        return nums[k - 1];
+    }
+};
