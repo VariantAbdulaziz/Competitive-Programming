57e5ea7ad1a2350a4433e38f3314e9333a0841ff
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-14 10:15:00 +0300

    solved insertion sort problem

---
+++ insertion_sort.cpp
@@ -0,0 +1,26 @@
+// source: https://www.hackerrank.com/challenges/insertionsort1/problem
+
+void print(int n, const vector<int>& arr){
+    
+    for(int i{}; i < n; i ++){
+        std::cout << arr[i] << " " ;
+    }
+}
+
+void insertionSort1(int n, vector<int> arr) {
+    
+    for(int i=1; i < n; i ++){
+        
+        int temp = arr[i];  
+        int j= i-1;  
+        while(j>=0 && temp <= arr[j])  
+        {  
+            arr[j+1] = arr[j];  
+            j = j-1;
+            print(n, arr);
+            std::cout << std::endl;
+        }
+        arr[j+1] = temp;
+    }
+    print(n, arr);
+}
