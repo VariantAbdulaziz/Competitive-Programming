4114244e1c842e764e46149bd7f591aff235bfd4
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-14 11:18:57 +0300

    implemented selection sort

---
+++ selection_sort.cpp
@@ -0,0 +1,25 @@
+// solution: https://practice.geeksforgeeks.org/problems/selection-sort/1
+
+class Solution{
+public:
+    int select(int arr[], int n, int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+        for(int j = i + 1; j < n; j ++){
+            if(arr[j] < arr[i]) i = j;
+        }
+        return i;
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+        
+       for(int i{}; i < n - 1; i++){
+           
+           int minElem = select(arr, n, i);
+           int temp = arr[minElem];
+           arr[minElem] = arr[i];
+           arr[i] = temp;
+       }
+    }
+};
