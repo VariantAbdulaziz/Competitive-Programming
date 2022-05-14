7958568c24a9fe9e12e73fd427a27307c94544fd
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-14 11:44:25 +0300

    solved smaller than cur number count problem

---
+++ smaller_than_cur.cpp
@@ -0,0 +1,27 @@
+// source: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/submissions/
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        
+        #define SIZE 101
+        int n = nums.size();
+        vector<int> count(SIZE, 0);
+        vector<int> preSum(SIZE + 1, 0);
+        vector<int> res(n);
+        
+        for(int i{}; i < n; i++){
+            count[nums[i]]++;
+        }
+        
+        for(int i{}; i < SIZE; i++){
+            preSum[i+1] = preSum[i] + count[i];
+        }
+        
+        for(int i{}; i < n; i++){
+            res[i] = preSum[nums[i]];
+        }
+        
+        return res;
+    }
+};
