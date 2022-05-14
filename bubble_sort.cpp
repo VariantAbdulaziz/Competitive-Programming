b22b3aa3f705c24aefdd5a4a0cac6523e1962838
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-14 08:49:14 +0300

    making bubble sort more funky

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,20 +1,23 @@
 // // source: https://www.hackerrank.com/challenges/ctci-bubble-sort/problem
 
 void countSwaps(vector<int> a) {
-    
+
     int n = a.size();
     int numSwaps{};
     for (int i = 0; i < n; i++) {
         
-        for (int j = 0; j < n - 1; j++) {
+        bool swapped = false;
+        for (int j = 0; j < n - 1 - i; j++) {
             // Swap adjacent elements if they are in decreasing order
             if (a[j] > a[j + 1]) {
                 int temp = a[j + 1];
                 a[j + 1] = a[j];
                 a[j] = temp;
                 numSwaps++;
+                swapped = true;
             }
         }
+        if(!swapped) break;
     }
     std::cout << "Array is sorted in " << numSwaps << " swaps." << std::endl;
     std::cout << "First Element: " << a[0] << std::endl;
