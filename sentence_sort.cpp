3ebe5634895b3a8e0a5d1b54ae62933226acfbfe
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-14 10:49:43 +0300

    solved sentence sortign problem

---
+++ sentence_sort.cpp
@@ -0,0 +1,29 @@
+// source: https://leetcode.com/problems/sorting-the-sentence/
+
+class Solution {
+public:
+    string sortSentence(string s) {
+        
+        stringstream ss(s);
+        vector<string> sv{};
+        string curW = "";
+        while(ss >> curW)
+            sv.push_back(curW);
+        sort(
+            sv.begin(),
+            sv.end(),
+            [](const string& a, const string& b){
+                return a.back() - '0' < b.back() - '0';
+            }
+        );
+          
+        string result = "";
+        for(string word: sv){
+            word.pop_back();
+            result += word + " ";
+        }
+        result.pop_back();
+        
+        return result;
+    }
+};
