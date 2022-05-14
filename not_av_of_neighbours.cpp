65bcd6423579c8902852586553f172b9a4dff452
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-14 16:08:33 +0300

    solved not average of neighbours

---
+++ not_av_of_neighbours.cpp
@@ -0,0 +1,28 @@
+// source: https://leetcode.com/problems/array-with-elements-not-equal-to-average-of-neighbors/
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        
+        sort(
+            nums.begin(),
+            nums.end(),
+            [](const int& a, const int&b){
+                
+                string sa = to_string(a);
+                string sb = to_string(b);
+                return sa + sb > sb + sa;
+            }
+        );
+        
+        if(nums[0]==0)
+            return "0";
+        
+        string res = "";
+        for(auto num: nums){
+            
+            res += to_string(num);
+        }
+        return res;
+    }
+};
