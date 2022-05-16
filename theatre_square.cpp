dfdc71634414f15e3727be5bd19ea79737e35cb2
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-16 16:18:48 +0300

    solution to the theatre square problem

---
+++ theatre_square.cpp
@@ -0,0 +1,12 @@
+// source: https://codeforces.com/problemset/problem/1/A
+
+// policy: an ad hoc solution
+#include <cmath>
+#include <iostream>
+    
+int main(){
+    unsigned long long m, n, a, ans;
+    std::cin >> m >> n >> a;
+    ans = ceil((double)m/a) * ceil((double)n/a);
+    std::cout << ans;
+}
