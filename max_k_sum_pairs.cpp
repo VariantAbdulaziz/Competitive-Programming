6928b87bb70899b56d7688d8f9f40e69c8a1ec8e
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-16 14:06:51 +0300

    solving maximum k sum pairs problem

---
+++ max_k_sum_pairs.cpp
@@ -0,0 +1,21 @@
+// source: https://leetcode.com/problems/max-number-of-k-sum-pairs/
+
+// policy: sorting | two pointers
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        
+        sort(nums.begin(), nums.end());
+        int l{}, r = nums.size() - 1, ans{};
+        while(l < r){
+            if(nums[r] + nums[l] == k){
+                ans++;
+                l++;
+                r--;
+            }
+            else if(nums[r] + nums[l] < k) l++;
+            else r--;
+        }
+        return ans;
+    }
+};
