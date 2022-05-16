9a4f0519151e033f1b7007d346f86f42b1ea1be2
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-16 12:07:15 +0300

    solved number of valleys problem

---
+++ num_valleys.cpp
@@ -0,0 +1,18 @@
+// source: https://www.hackerrank.com/challenges/counting-valleys/problem
+
+// policy: semaphores
+int countingValleys(int steps, string path) {
+    int numVals{}, height{}, semaphore{};
+    for(auto dir: path){
+        if(dir == 'D'){
+            if(height <= 0) semaphore++;
+            height--;
+        }else {
+            if(height < 0) semaphore--;
+            height++;
+            if(height == 0 and semaphore == 0)
+                numVals++;
+        }
+    }
+    return numVals;
+}
