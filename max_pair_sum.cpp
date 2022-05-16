e6531a91bbf463d369f2eccf1e26dd314bb64902
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-16 15:19:57 +0300

    solved max pair sum problem

---
+++ max_pair_sum.cpp
@@ -0,0 +1,15 @@
+// source: https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/
+
+// policy: sorting | two pointers
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        int maxSum = nums[0] + nums[nums.size() - 1];
+        for(int i{}; i < nums.size()/2; i++){
+            maxSum = max(maxSum, nums[i] + nums[nums.size() - 1 - i]);
+        }
+        return maxSum;
+    }
+};
