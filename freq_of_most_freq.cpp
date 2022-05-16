6c3c9257605954da52fef0733b9798c69529f172
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-16 11:01:24 +0300

    solved frequency of most frequent element problem

---
+++ freq_of_most_freq.cpp
@@ -0,0 +1,34 @@
+// source: https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+
+// policy: sort | prefix sum | sliding widnow
+class Solution {
+public:
+    void prefixSum(const vector<int>& nums, vector<long long>& preSum){
+        preSum[0] = 0;
+        for(int i{}; i < nums.size(); i++){
+            preSum[i + 1] = preSum[i] + nums[i];
+        }
+    }
+    
+    int maxFrequency(vector<int>& nums, int k) {
+        
+        vector<long long> preSum(nums.size() + 1);
+        
+        sort(nums.begin(), nums.end());
+        prefixSum(nums, preSum);
+        
+        long long freq{}, l{}, r=1;
+        while (r < preSum.size() ) {
+
+            if((nums[r-1] * (r - l)) - (preSum[r] - preSum[l]) <= k){
+                freq = max(freq, r - l);
+                r++;
+            }else {
+                l++;
+            }
+                
+        }
+        
+        return freq;
+    }
+};
