9a645b811a092df19e04ec6024c7a26ea08f0cb5
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-17 18:15:39 +0300

    solved the  reduce array to halve prob with heap

--- halve_arr_size.cpp
+++ halve_arr_size.cpp
@@ -1,28 +1,23 @@
 // source: https://leetcode.com/problems/reduce-array-size-to-the-half/submissions/
 
-// policy: frequency distribution
+// policy: frequency distribution | priority queue
 class Solution {
 public:
-    #define MAX_ARR_ELEM 100'001
     int minSetSize(vector<int>& arr) {
-        vector<int> freq(MAX_ARR_ELEM, 0);
+        unordered_map<int, int> freq;
+        priority_queue<int> maxH;
         
-        for(auto x: arr)
-            freq[x]++;
+        for(auto val: arr)
+            freq[val]++;
         
-        sort(
-            freq.begin(),
-            freq.end(),
-            [](auto& l, auto& r){
-                return l > r;
-            }
-        );
-        int cur_sum = 0;
-        for(int i{}; i < freq.size(); i++){
-            cur_sum += freq[i];
-            if (cur_sum >= arr.size()/2)
-                return i + 1;
+        for(auto [key, val]: freq)
+            maxH.push(val);
+        
+        int preFreqSum = 0, minSetLen = 0;
+        while(preFreqSum < arr.size()/2 && !maxH.empty()){
+            minSetLen++;
+            preFreqSum += maxH.top(); maxH.pop();
         }
-        return 0;
+        return minSetLen;
     }
 };
