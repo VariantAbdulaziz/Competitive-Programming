4fce87b9e584e08bdfc77ccec46a2a7815d2b355
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-17 20:32:15 +0300

    evaluated reverse polish notation

---
+++ rpn.cpp
@@ -0,0 +1,29 @@
+// source: https://leetcode.com/problems/evaluate-reverse-polish-notation
+
+// policy: stack
+class Solution {
+public:
+    int compute(int l, int r, string op){
+        if(op == "+") 
+            return l + r;
+        else if(op == "-") 
+            return l - r;
+        else if(op == "*") 
+            return l * r;
+        else
+            return l / r;
+    }
+    int evalRPN(vector<string>& tokens) {
+        set<string> ops{"+", "-", "*", "/"};
+        stack<int> aStack;
+        for(auto token: tokens){
+            
+            if(ops.count(token)){
+                int r = aStack.top(); aStack.pop();
+                int l = aStack.top(); aStack.pop();
+                aStack.push(compute(l, r, token));
+            } else aStack.push(stoi(token));
+        }
+        return aStack.top();
+    }
+};
