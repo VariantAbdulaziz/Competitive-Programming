f59cbc056eaad392d7686899c6ca59b9042cb360
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-17 20:03:45 +0300

    implemented a basic queue with two stacks

---
+++ q_two_stack_tale.cpp
@@ -0,0 +1,57 @@
+// source: https://leetcode.com/problems/implement-queue-using-stacks/
+
+// policy: stack
+class MyQueue {
+public:
+    MyQueue() {
+        stack<int> first;
+        stack<int> second;
+    }
+    
+    void push(int x) {
+        first.push(x);
+    }
+    
+    int pop() {
+        while(!first.empty()){
+            second.push(first.top());
+            first.pop();
+        }
+        int popped = second.top();
+        second.pop();
+        while(!second.empty()){
+            first.push(second.top());
+            second.pop();
+        }
+        return popped;
+    }
+    
+    int peek() {
+        while(!first.empty()){
+            second.push(first.top());
+            first.pop();
+        }
+        int popped = second.top();
+        while(!second.empty()){
+            first.push(second.top());
+            second.pop();
+        }
+        return popped;
+    }
+    
+    bool empty() {
+        return first.empty();
+    }
+private:
+    stack<int> first;
+    stack<int> second;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
