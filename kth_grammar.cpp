f657f3a48ec93acbe0dcb691b380c5ea9c31428c
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-25 13:57:31 +0300

    solved kth grammar problem

---
+++ kth_grammar.cpp
@@ -0,0 +1,15 @@
+// source: https://leetcode.com/problems/k-th-symbol-in-grammar/
+
+// strategy: recursion
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n == 1 && k == 1){
+            return 0;
+        }
+        int mid = pow(2, n - 1)/2;
+        if(k <= mid)
+            return kthGrammar(n - 1, k);
+        return !kthGrammar(n - 1, k - mid);
+    }
+};
