592ddef5f798c93f99512b19a8085633e7814d1b
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-10-17 10:45:34 +0300

    Time: 872 ms (5.00%), Space: 8.6 MB (6.42%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool PredictTheWinner(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<vector<int>>> cache(n, vector<vector<int>>(n, {-1, -1}));
-        auto dp = [&](auto&& dp, int l, int r, int turn){
+    int dp(vector<vector<vector<int>>>& cache, vector<int>&nums, int l, int r, int turn){
             if(r < l) return 0;
             
             auto& res = cache[l][r][turn];
             if(res != -1) return res;
-            if(turn) return max(nums[l] + dp(dp, l+1, r, !turn), nums[r] + dp(dp, l, r-1, !turn));
+            if(turn) return max(nums[l] + dp(cache, nums, l+1, r, !turn), nums[r] + dp(cache, nums, l, r-1, !turn));
             
-            return min(dp(dp, l+1, r, !turn), dp(dp, l, r-1, !turn));
-        };
+            return min(dp(cache, nums, l+1, r, !turn), dp(cache, nums, l, r-1, !turn));
+    }
+    bool PredictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<vector<int>>> cache(n, vector<vector<int>>(n, {-1, -1}));
         int total = 0;
         for(int ni: nums) total += ni;
-        int score = dp(dp, 0, n-1, 1);
+        int score = dp(cache, nums, 0, n-1, 1);
         int anti_score = total - score;
         return score >= anti_score? true: false;
     }
