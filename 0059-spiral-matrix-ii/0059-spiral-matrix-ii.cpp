f075597eaf397f8bb2485d08b33b9974ad99397e
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-10-18 15:31:45 +0300

    Time: 0 ms (100.00%), Space: 6.6 MB (63.20%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> spiral_matrix(n, vector<int>(n, -1));
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int x=0;
+        int y=0;
+        int cur_direction=0;
+        int to_be_populated = 1;
+        
+        while(in_bound(x, y, n) && !is_populated(x, y, spiral_matrix)){
+            auto [dx, dy] = directions[cur_direction];
+
+            while(in_bound(x, y, n) && !is_populated(x, y, spiral_matrix)){
+                spiral_matrix[x][y] = to_be_populated++;
+                x += dx;
+                y += dy;
+            }
+            // retracting our last update to x and y
+            x -= dx;
+            y -= dy;
+            
+            // change direction
+            cur_direction = (cur_direction + 1) % 4;
+            dx = directions[cur_direction].first;
+            dy = directions[cur_direction].second;
+            
+            // update x and y with next direction
+            x += dx;
+            y += dy;
+        }
+        
+        return spiral_matrix;
+    }
+private:
+    bool in_bound(int x, int y, int n){
+        return x >=0 && x < n && y >= 0 && y < n;
+    }
+    bool is_populated(int x, int y, vector<vector<int>>& spiral_matrix){
+        return spiral_matrix[x][y] != -1;
+    }
+};
