2115f12d568f461f1aae8dc740be558c69d0c535
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-10-21 05:30:39 +0300

    Time: 540 ms (42.54%), Space: 105.2 MB (67.35%) - LeetHub

---
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -0,0 +1,45 @@
+class RandomizedCollection {
+public:
+    RandomizedCollection() {
+    
+    }
+    
+    bool insert(int val) {
+        int idx = bucket.size();
+        bucket.push_back(val);
+        book[val].insert(idx);
+        return book[val].size() == 1;
+    }
+    
+    bool remove(int val) {
+        if(book.find(val) == book.end()) return false;
+        
+        int last = bucket.size()-1;
+        auto pivot = *book[val].begin();
+        book[val].erase(pivot);
+        
+        swap(bucket[pivot], bucket[last]);
+        
+        book[bucket[pivot]].insert(pivot);
+        book[bucket[pivot]].erase(last);
+        bucket.pop_back();
+        
+        if(book[val].empty()) book.erase(val);
+        
+        return true;
+    }
+    
+    int getRandom() {
+        return bucket[rand()%bucket.size()];
+    }
+    vector<int> bucket;
+    unordered_map<int, set<int>> book;
+};
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
