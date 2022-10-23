7f2001580d6027b8ec13de503dbf23c249d98d84
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-10-23 12:12:28 +0300

    Time: 263 ms (62.41%), Space: 79.1 MB (26.32%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -7,11 +7,9 @@ public:
             int l = r - indexDiff - 1;
             if(l >= 0 && window[nums[l]] == l) window.erase(nums[l]);
             
-            auto down_pos = window.lower_bound(nums[r] - valueDiff);
-            auto up_pos = window.lower_bound(nums[r] + valueDiff);
+            auto pos = window.lower_bound(nums[r] - valueDiff);
             
-            if(down_pos != window.end() && abs((long long)(*down_pos).first - nums[r]) <= valueDiff) return true;
-            if(up_pos != window.end() && abs((long long)(*up_pos).first - nums[r]) <= valueDiff) return true;
+            if(pos != window.end() && abs((long long)(*pos).first - nums[r]) <= valueDiff) return true;
             window[nums[r]] = r;
         }
         return false;
