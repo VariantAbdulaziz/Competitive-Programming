70fc223a325ee87333294abab9c6a019fb1520ba
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-10-26 16:26:20 +0300

    Time: 7 ms (37.25%), Space: 6.4 MB (77.65%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     int findMinFibonacciNumbers(int k) {
         vector<int> book{1, 1};
-        map<int, int> cache;
         
         int x = 2;
         while(x <= k){
             book.push_back(x);
             x = book[book.size() - 1] + book[book.size() - 2];
         }
-        return FibConstitution(cache, book, k);
+        return FibConstitution(book, k);
     }
     
-    int FibConstitution(map<int, int>& cache, vector<int> &book, int ki){
-        if(cache.find(ki) != cache.end()) return cache[ki];
-        
-        auto &result = cache[ki];
+    int FibConstitution(vector<int> &book, int ki){
         int station = 0;
         for(int jump = ki/2; jump > 0; jump /= 2){
             while(station + jump < book.size() && book[station + jump] <= ki)
                 station += jump;
         }
         
-        if(book[station] == ki) result = 1;
-        else result = 1 + FibConstitution(cache, book, ki - book[station]);
-        return result;
+        if(book[station] == ki) return 1;
+        return 1 + FibConstitution(book, ki - book[station]);
     }
 };
