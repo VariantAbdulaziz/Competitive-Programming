358553197e140b7cd551e751550990a512776696
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-10-27 14:13:53 +0300

    Time: 15 ms (57.71%), Space: 9.1 MB (36.03%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int find(vector<int>& link, int p){
-        if(p != link[p]){
-            link[p] = find(link, link[p]);
-        }
-        return link[p];
-    }
-    bool cluster(vector<int>& link, vector<int>& rank, int l, int r){
-        int lr = find(link, l);
-        int rr = find(link, r);
-        if(lr == rr) return false;
-        if(rank[lr] < rank[rr]) swap(lr, rr);
-        rank[lr] += rank[rr];
-        link[rr] = lr;
-        return true;
-    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         // for each edge query if it is redundant or not(if there already exist that connection or not)
-        vector<int> link(edges.size() + 1); iota(begin(link), end(link), 0);
+        vector<int> link(edges.size() + 1);
+        int count = 0;
+        generate(link.begin(), link.end(), [&](){return count++;});
         vector<int> rank(edges.size() + 1, 1);
         
+        auto find = [&](auto&& find, int p) -> int{
+            if(p != link[p]){
+                link[p] = find(find, link[p]);
+            }
+            return link[p];
+        };
+        auto cluster = [&](int l, int r){
+            int lr = find(find, l);
+            int rr = find(find, r);
+            if(lr == rr) return false;
+            if(rank[lr] < rank[rr])
+                swap(lr, rr);
+
+            rank[lr] += rank[rr];
+            link[rr] = lr;
+            return true;
+        };
+        
         for(auto edge: edges){
-            if(!cluster(link, rank, edge[0], edge[1])) return edge;
+            if(!cluster(edge[0], edge[1])) return edge;
         }
         return vector<int>();
     }
