d618d3d2c31295980144021a2636a1591ae570b0
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-10-27 12:21:40 +0300

    Time: 10 ms (82.33%), Space: 9 MB (48.05%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int find(vector<int>& link, int p){
+        if(p != link[p]){
+            link[p] = find(link, link[p]);
+        }
+        return link[p];
+    }
+    bool unite(vector<int>& link, vector<int>& rank, int l, int r){
+        int lr = find(link, l);
+        int rr = find(link, r);
+        if(lr == rr) return false;
+        if(rank[lr] < rank[rr]) swap(lr, rr);
+        rank[lr] += rank[rr];
+        link[rr] = lr;
+        return true;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        // for each edge query if it is redundant or not(if there already exist that connection or not)
+        vector<int> link(edges.size() + 1); iota(begin(link), end(link), 0);
+        vector<int> rank(edges.size() + 1, 1);
+        
+        for(auto edge: edges){
+            if(!unite(link, rank, edge[0], edge[1])) return edge;
+        }
+        return vector<int>();
+    }
+};
