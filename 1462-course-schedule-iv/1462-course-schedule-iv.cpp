1b9ee31ce843a79361a2f44e2a961aa72b9b27f6
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-10-27 11:12:52 +0300

    Time: 2653 ms (5.01%), Space: 69.1 MB (27.68%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<vector<int>> connections(numCourses, vector<int>(numCourses, 0));
+        deque<deque<bool>> connections(numCourses, deque<bool>(numCourses, false));
         
         for(auto edge: prerequisites){
             int a = edge[0], b = edge[1];
-            connections[a][b] = 1;
+            connections[a][b] = true;
         }
         for(int k=0; k<numCourses; k++){
             for(int i=0; i<numCourses; i++){
