55525ed1df8dc4836ca50c1207d6c77071880f65
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-11-04 10:33:45 +0300

    Time: 549 ms (19.68%), Space: 130.6 MB (5.70%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        
+        int result = 0;
         vector<int> is_prime(n, 1);
         for(int i=2; i<n; i++){
+            result += is_prime[i];
             if(is_prime[i])
                 for(long long j=(long long)i*i; j<n; j+=i){
                     is_prime[j] = 0;
                 }
         }
-        int result = 0;
-        for(int i=2; i<n; i++){
-            result += is_prime[i];
-        }
+
         return result;
     }
 };
