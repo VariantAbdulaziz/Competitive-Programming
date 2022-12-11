b0a93f1dce1134256fda7c980ce04f3ec78f2c4b
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-12-11 05:24:51 +0300

    Time: 4 ms (43.20%), Space: 7.9 MB (25.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,10 +4,12 @@ public:
         int n = nums.size();
         vector<int> dp(n+2);
         
-        for(int i=0; i<n; i++){
-            for(int j=i+2; j-2<n; j++){
-                dp[j] = max(dp[j], dp[i] + nums[i]);
-            }
+        for(int i=0; i<n; i++) {
+            dp[i+2] = max(dp[i+2], dp[i] + nums[i]);
+            dp[i+1] = max(dp[i+1], dp[i]);
+            // for(int j=i+2; j-2<n; j++){
+            //     dp[j] = max(dp[j], dp[i] + nums[i]);
+            // }
         }
         return max(dp[n], dp[n+1]);
     }
