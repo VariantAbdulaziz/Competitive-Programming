297e529c5889708a17a063ef9991c181bd55feec
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2022-12-21 13:21:21 +0300

    Time: 5 ms (36.12%), Space: 6 MB (80.19%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     int numTilings(int n) {
         const int mod = 1e9 + 7;
-        vector<long> full(n + 2), partial(n + 2);
-        full[0] = 1;
+        long curr_full = 1, curr_partial = 0;
+        long next_full = 0, next_partial = 0, next_next_partial = 0, next_next_full = 0;
         for(int i=0; i<n; i++){
-            full[i] %= mod;
-            partial[i] %= mod;
-            full[i + 1] += full[i];
-            full[i + 2] += full[i];
-            full[i + 1] += partial[i];
-            partial[i + 2] += 2 * full[i];
-            partial[i + 1] += partial[i];
+            curr_full %= mod;
+            curr_partial %= mod;
+            next_full += curr_full;
+            next_next_full += curr_full;
+            next_full += curr_partial;
+            next_next_partial += 2 * curr_full;
+            next_partial += curr_partial;
+            
+            curr_full = next_full;
+            next_full = next_next_full;
+            next_next_full = 0;
+            curr_partial = next_partial;
+            next_partial = next_next_partial;
+            next_next_partial = 0;
         }
-        return full[n] % mod;
+        return curr_full % mod;
     }
 };
