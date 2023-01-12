27da8d8a9e0bfa974aabc3a858745f21b777a3da
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-01-12 14:24:07 +0300

    Time: 156 ms (85.08%), Space: 65.4 MB (68.47%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxJump(vector<int>& stones) {
+        int res = stones[1] - stones[0], n = stones.size();
+        for (int i = 2; i < n; ++i)
+            res = max(res, stones[i] - stones[i - 2]);
+        return res;
+    }
+};
