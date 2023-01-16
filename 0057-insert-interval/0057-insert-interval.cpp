64f06349753468284eab306b2f6dc777c2643f12
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-01-16 20:43:31 +0300

    Time: 308 ms (5.01%), Space: 56.7 MB (5.23%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         intervals.push_back(newInterval);
-        sort(begin(intervals), end(intervals));
+        sort(begin(intervals), end(intervals), [](auto a, auto b){
+            return a[0] < b[0] || (a[0] == b[0] && a[1] < b[1]);
+        });
         vector<vector<int>> result;
         for(auto interval : intervals){
             if(!result.empty() && interval[0] <= result.back()[1]){
