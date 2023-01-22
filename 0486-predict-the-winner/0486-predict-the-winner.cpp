a4eaccc70b2cee5817f817710fd61e0215558bbf
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-01-22 18:14:12 +0300

    Time: 7 ms (46.49%), Space: 8.6 MB (7.45%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
         int N = nums.size();
-        vector<vector<vector<int64_t>>> dp(N, vector<vector<int64_t>>(N, {0, 0}));
+        vector<vector<vector<long>>> dp(N, vector<vector<int64_t>>(N, {0, 0}));
         for(int len = 1; len <= N; len++){
             for(int l = 0; l + len <= N; l++){
                 for(int turn = 0; turn < 2; turn++){
@@ -17,10 +17,10 @@ public:
                 }
             }
         }
-        int64_t total = 0;
+        long total = 0;
         for(auto num: nums) total += num;
-        int64_t player_one_score = dp[0][N-1][1];
-        int64_t player_two_score = total - player_one_score;
+        long player_one_score = dp[0][N-1][1];
+        long player_two_score = total - player_one_score;
         return player_one_score >= player_two_score;
     }
 };
