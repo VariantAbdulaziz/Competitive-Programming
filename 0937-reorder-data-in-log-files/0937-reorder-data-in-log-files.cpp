55aa361be9f7afa0f17996098cc22cab1143afd8
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-02-02 01:33:03 +0300

    Time: 31 ms (15.29%), Space: 16.3 MB (5.03%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.cpp
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.cpp
@@ -14,14 +14,14 @@ public:
             }
             seeker--;
         }
-        sort(rend(logs) - holder - 1, rend(logs), [](auto a, auto b){
+        sort(begin(logs), end(logs) - (N - holder - 1), [](auto a, auto b){
             stringstream ssa(a), ssb(b);
             string identifier_a, identifier_b, content_a, content_b;
             ssa >> identifier_a;
             content_a = a.substr(identifier_a.size() + 1);
             ssb >> identifier_b;
             content_b = b.substr(identifier_b.size() + 1);
-            return content_a > content_b || (content_a == content_b && identifier_a >= identifier_b);
+            return content_a < content_b || (content_a == content_b && identifier_a <= identifier_b);
         });
         return logs;
     }
