551a442a90d991df51458c4119822edb16a80632
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-02-02 00:36:24 +0300

    Time: 147 ms (67.23%), Space: 24.5 MB (59.33%) - LeetHub

---
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long numberOfWays(string s) {
+        int N = s.size();
+        long number_of_ways = 0;
+        vector<long long> middle(2), first_order(2); 
+        for(int i = 0; i < N; i++){
+            number_of_ways += middle['1' - s[i]];
+            middle[s[i] - '0'] += first_order['1' - s[i]];
+            first_order[s[i] - '0']++;
+        }
+        return number_of_ways;
+    }
+};
