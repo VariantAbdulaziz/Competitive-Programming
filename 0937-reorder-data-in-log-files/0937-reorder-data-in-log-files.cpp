baf58b1d5f5abf61124f83283ae235cab939d2ca
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-02-02 01:20:37 +0300

    Time: 27 ms (23.34%), Space: 15.6 MB (7.85%) - LeetHub

---
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        int N = logs.size();
+        int seeker = N - 1, holder = N - 1;
+        while(seeker >= 0){
+            string identifier, word;
+            stringstream ss(logs[seeker]);
+            ss >> identifier;
+            ss >> word;
+            if(isdigit(word[0])){
+                swap(logs[holder], logs[seeker]);
+                holder--;
+            }
+            seeker--;
+        }
+        sort(rend(logs) - holder - 1, rend(logs), [](auto a, auto b){
+            stringstream ssa(a), ssb(b);
+            string identifier_a, identifier_b, content_a, content_b;
+            ssa >> identifier_a;
+            content_a = a.substr(identifier_a.size() + 1);
+            ssb >> identifier_b;
+            content_b = b.substr(identifier_b.size() + 1);
+            return content_a > content_b || (content_a == content_b && identifier_a >= identifier_b);
+        });
+        return logs;
+    }
+};
