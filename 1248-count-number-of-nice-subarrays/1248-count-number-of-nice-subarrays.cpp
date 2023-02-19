55271a41c8c2245c44eab5a4825af2a6c1c3cd3e
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-02-19 10:34:13 +0300

    Time: 153 ms (65.03%), Space: 70.9 MB (49.78%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n = nums.size(), ans = 0, odds = 0;
-        unordered_map<int, int> book;
+        vector<int> book(n + 1);
         book[0]++;
         for(int i=0; i<n; i++) {
             odds += nums[i] % 2? 1: 0;
-            ans += book[odds - k];
+            ans += odds - k >= 0? book[odds - k]: 0;
             book[odds]++;
         }
         return ans;
