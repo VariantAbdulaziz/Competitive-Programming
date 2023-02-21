32e5f301e8d2fb04835f8a29cdf576b1029123b5
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-02-21 21:50:47 +0300

    Time: 4 ms (98.14%), Space: 12.1 MB (68.94%) - LeetHub

---
+++ 0385-mini-parser/0385-mini-parser.cpp
@@ -0,0 +1,63 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    NestedInteger deserialize(string s) {
+        int idx = 0;
+        if(s[idx] == '[')
+            return deserializeNested(s, idx);
+        return deserializeSingle(s, idx);
+    }
+    
+    NestedInteger deserializeNested(string &s, int &idx) {
+        idx++;
+        NestedInteger root;
+        int n = s.size();
+        while (idx < n && s[idx] != ']') {
+            if (s[idx] == '[') {
+                root.add(deserializeNested(s, idx));
+            } else if (isdigit(s[idx]) || s[idx] == '-') {
+                root.add(deserializeSingle(s, idx));
+            } else if (s[idx] == ',') idx++;
+        }
+        idx++;
+        return root;
+    }
+    
+    NestedInteger deserializeSingle(string &s, int &idx) {
+        int n = s.size(), j = idx;
+        while(j < n && (isdigit(s[j]) || s[j] == '-')) {
+            j += 1;
+        }
+        auto root = NestedInteger(stoi(s.substr(idx, j - idx)));
+        idx = j;
+        return root;
+    }
+};
