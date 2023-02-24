12fc32eef2dfb58213e7cdd6a59013317950ad7a
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-02-24 03:37:49 +0300

    Time: 574 ms (37.16%), Space: 10.2 MB (83.41%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -8,10 +8,7 @@ public:
         for(int len = 0; len < n; len++) {
             for(int l = 0; l + len < n; l++) {
                 int r = l + len;
-                if(l == r) {
-                    dp[l][r] = (l - 1 < 0? 1: nums[l - 1]) * nums[l] * (r + 1 == n? 1: nums[r + 1]);
-                    continue;
-                }
+
                 for(int i=l; i<=r; i++) {
                     int coins = (l - 1 < 0? 1: nums[l - 1]) * nums[i] * (r + 1 == n? 1: nums[r + 1]);
                     coins += (i - 1 < 0? 0: dp[l][i - 1]) + (i + 1 == n? 0: dp[i + 1][r]);
