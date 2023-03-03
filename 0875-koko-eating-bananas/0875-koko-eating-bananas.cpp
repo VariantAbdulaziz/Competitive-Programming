760979d7d063e66a1f51534aab82b6aea2e4c345
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-03-03 17:13:23 +0300

    Time: 52 ms (60.87%), Space: 18.9 MB (43.99%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -10,7 +10,7 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int _max = *max_element(begin(piles), end(piles));
         int k = _max;
-        for(int j=(_max + 1)/2; j > 0; j /= 2) {
+        for(int j=(_max)/2; j > 0; j /= 2) {
             while(k - j > 0 && valid(piles, k - j, h)) {
                 k -= j;
             }
