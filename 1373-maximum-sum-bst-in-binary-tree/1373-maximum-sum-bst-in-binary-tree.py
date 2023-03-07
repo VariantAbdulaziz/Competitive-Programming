c6abc720e81049046b606d9f9250fed544aead8a
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-03-07 22:12:01 +0300

    Time: 403 ms (76.69%), Space: 67.8 MB (37.59%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -6,29 +6,25 @@
 #         self.right = right
 class Solution:
     def maxSumBST(self, root: Optional[TreeNode]) -> int:
-        book = {}
         ans = -float('inf')
         def populate(root):
             nonlocal ans
-            if not root:
-                return
             if not root.left and not root.right:
                 ans = max(ans, root.val)
-                book[root] = (root.val, True, root.val, root.val)
-                return
+                return (root.val, True, root.val, root.val)
             
-            populate(root.left)
-            populate(root.right)
+            
+            
             curr_sum, curr_is_bst, curr_min, curr_max = root.val, True, root.val, root.val
             if root.left:
-                left_sum, left_is_bst, left_min, left_max = book[root.left]
+                left_sum, left_is_bst, left_min, left_max = populate(root.left)
                 curr_sum += left_sum
                 curr_is_bst = curr_is_bst and left_is_bst and left_max < root.val 
                 curr_min = min(curr_min, left_min)
                 curr_max = max(curr_max, left_max)
             
             if root.right:
-                right_sum, right_is_bst, right_min, right_max = book[root.right]
+                right_sum, right_is_bst, right_min, right_max = populate(root.right)
                 curr_sum += right_sum
                 curr_is_bst = curr_is_bst and right_is_bst and right_min > root.val
                 curr_min = min(curr_min, right_min)
@@ -36,7 +32,7 @@ class Solution:
             
             if curr_is_bst:
                 ans = max(ans, curr_sum)
-            book[root] = curr_sum, curr_is_bst, curr_min, curr_max
+            return (curr_sum, curr_is_bst, curr_min, curr_max)
             
         populate(root)
         return max(ans, 0)
