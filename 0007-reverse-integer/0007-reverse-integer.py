f2e742cb35d6ee53806a35273dfdc2ac6d737da8
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-05-26 18:20:11 +0300

    Time: 45 ms (38.28%), Space: 16.3 MB (8.89%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -3,13 +3,8 @@ class Solution:
         if x == 0: return x
         sign = x // abs(x)
         x = abs(x)
-        result = 0
-        while x != 0:
-            lsd = x % 10
-            result *= 10 # 3*10 + lsd
-            result += lsd
-            x //= 10
-            
+        strx = str(x)
+        result = int(strx[::-1])
         result = sign * result
         if result < -2**31 or result > (2**31 - 1):
             result = 0
