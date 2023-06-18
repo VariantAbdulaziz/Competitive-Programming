1094e749020204dda62f0d06e18cc75a98e937bf
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-06-18 11:28:44 +0300

    Time: 57 ms (68.48%), Space: 16.4 MB (43.14%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,13 +1,14 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        n = len(s)
-        m = len(p)
-        dp = [[False] * (m + 1) for _ in range(n + 1)]
+        m, n = len(s), len(p)
+        dp = [[False] * (n+1) for _ in range(m+1)]
         dp[0][0] = True
-        for i in range(n + 1):
-            for j in range(1, m + 1):
+
+        for i in range(m+1):
+            for j in range(1, n+1):
                 if j > 1 and p[j-1] == '*':
-                    dp[i][j] = dp[i][j-2] or (i > 0 and (p[j-2] == '.' or p[j-2] == s[i-1]) and dp[i-1][j])
+                    dp[i][j] = dp[i][j-2] or (i > 0 and (p[j-2] == '.' or s[i-1] == p[j-2]) and dp[i-1][j])
                 else:
-                    dp[i][j] = i > 0 and dp[i-1][j-1] and (p[j-1] == '.' or p[j-1] == s[i-1])
-        return dp[n][m]
+                    dp[i][j] = i > 0 and dp[i-1][j-1] and (p[j-1] == '.' or s[i-1] == p[j-1])
+
+        return dp[m][n]
